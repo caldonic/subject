007399396b8fdeb5fe860cfc72b89bc9fa7a9271
Ely HSU <map60915@gmail.com>
2022-07-29 17:37:59 +0800

    feat 新增servlet+Hibernate環境

---
+++ src/main/java/model/hibernate/OpenSessionInViewFilter.java
@@ -0,0 +1,55 @@
+package model.hibernate;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+@WebFilter(
+		urlPatterns = {"/*"}
+		)
+public class OpenSessionInViewFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.getCurrentSession();
+		Transaction transaction = null;
+		try {
+			transaction = session.beginTransaction();
+			chain.doFilter(request, response); // 呼叫後端Servlet
+			transaction.commit();
+			
+		} catch (Exception e) {
+			transaction.rollback();
+			chain.doFilter(request, response);
+			e.printStackTrace();
+		
+		}finally {
+			session.close();
+		}
+		
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
