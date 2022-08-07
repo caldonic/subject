fee5182e8cdcc29481a5a50b12fda7e26509c0a2
Ely HSU <map60915@gmail.com>
2022-08-07 16:30:31 +0800

    feat  add CartServlet.java

--- src/main/java/subject/controller/CartServlet.java
+++ src/main/java/subject/controller/CartServlet.java
@@ -22,18 +22,20 @@ public class CartServlet extends HttpServlet {
 		res.setContentType("application/json;charset=UTF-8");
 		Gson gson = new Gson();
 		Cart cart = gson.fromJson(req.getReader(), Cart.class);
-		
-		System.out.println("couponname=" + cart.couponname);    //測試couponname接收
-		System.out.println("goldremaining=" + cart.goldremaining);//測試購物金接收
 
-		for (Cartlist cartlist : cart.cartlist) { //測試商品接收(疊代用法)
+		System.out.println("couponname=" + cart.couponname); // 測試couponname接收
+		System.out.println("goldremaining=" + cart.goldremaining);// 測試購物金接收
+
+		for (Cartlist cartlist : cart.cartlist) { // 測試商品接收(疊代用法)
 			System.out.println("cartobj=" + cartlist);
+//			System.out.println("cart.itemserialnumber=" + cartlist.itemserialnumber);
 		}
+
 //		for (int i = 0; i < cart.cartlist.size(); i++) {     //一般用法
 //			System.out.println("cartobj=" + cart.cartlist);
 //
 //		}
-		
+
 //	    =======訂單生成於資料庫區============
 //		try {
 //			OrderMainFileHibernateService orderMainFileHibernateService = new OrderMainFileHibernateServiceImpl();
@@ -42,14 +44,28 @@ public class CartServlet extends HttpServlet {
 //		} catch (Exception e) {
 //			e.printStackTrace();
 //		}
+		Integer itemserialnumber=null;
+		String itemname=null;
+		Integer itemquantity=null;
+		Integer itemprice=null;
+		for (Cartlist cartlist : cart.cartlist) { // 測試商品接收(疊代用法)
+			System.out.println("cartobj=" + cartlist);
+//			System.out.println("cart.itemserialnumber=" + cartlist.itemserialnumber);
+			itemserialnumber = cartlist.itemserialnumber;
+			itemname = cartlist.itemname;
+			itemquantity = cartlist.itemquantity;
+			itemprice = cartlist.itemprice;
+		}
+
 		try {
-			 OrderMainFileServiceNew orderMainFileServiceNew = new OrderMainFileServiceNew();
-		String orderresultanddetail=orderMainFileServiceNew.insertWithOrderDetail(cart.couponname);
-		System.out.println("orderresultanddetail="+orderresultanddetail);
-		}catch (Exception e) {
+			OrderMainFileServiceNew orderMainFileServiceNew = new OrderMainFileServiceNew();
+			String orderresultanddetail = orderMainFileServiceNew.insertWithOrderDetail(cart.couponname,
+					itemserialnumber); //, itemname,itemquantity,itemprice
+			System.out.println("orderresultanddetail=" + orderresultanddetail);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 //	    ======訂單生成於資料庫區結束========
-		
+
 	}
 }
