ecfc115f9d146b561685b342f0857ca6280d89e7
Ely HSU <map60915@gmail.com>
2022-08-11 14:59:58 +0800

    更新pom 郵件功能

--- src/main/java/subject/controller/CartServlet.java
+++ src/main/java/subject/controller/CartServlet.java
@@ -1,7 +1,6 @@
 package subject.controller;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,11 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.orderMainFile.model.OrderMailService;
+import com.orderMainFile.model.OrderMainFileDAO;
 
 import subject.service.OrderMainFileHibernateService;
 import subject.service.impl.OrderMainFileHibernateServiceImpl;
 import suject.cart.Cart;
-import suject.cart.Cartlist;
 
 @WebServlet("/CartServlet")
 public class CartServlet extends HttpServlet {
@@ -42,15 +42,20 @@ public class CartServlet extends HttpServlet {
 //	    =======訂單生成於資料庫區============
 		try {
 			OrderMainFileHibernateService orderMainFileHibernateService = new OrderMainFileHibernateServiceImpl();
-			String orderresult = orderMainFileHibernateService.orderMaininsert(cart.couponname, cart,cart.cartlist,cart.carttotal);
+			String orderresult = orderMainFileHibernateService.orderMaininsert(cart.couponname, cart, cart.cartlist,
+					cart.carttotal);
 			System.out.println("orderresult資料庫=" + orderresult);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 //	    ======訂單生成於資料庫區結束========
-		
+
 //       郵件
-		
+		OrderMainFileDAO orderMainFileDAO = new OrderMainFileDAO();
+		OrderMailService MAIL = new OrderMailService();
+
+		MAIL.sendMail("letitgo202203@gmail.com", "訂單通知", "您好！我們已收到您的訂單付款" + "<br>" + "訂單編號: "
+				+ (orderMainFileDAO.getOrderMail() + 1) + "<br>" + " 感謝您於 LETIGO 購物！");
 
 	}
 }
